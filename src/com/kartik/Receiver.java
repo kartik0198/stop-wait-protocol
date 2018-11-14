6958f6b1b74a8fe211d1b016bd80fc2c148903b8
Kartik Singhal <kartiksinghal0198@gmail.com>
2018-11-14 11:24:02 +0530

    Randomised the acknowledgement process

--- src/com/kartik/Receiver.java
+++ src/com/kartik/Receiver.java
@@ -4,6 +4,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class Receiver {
     private ServerSocket receiver;
@@ -12,8 +13,7 @@ public class Receiver {
     private String packet, data = "";
     private int i = 0;
     private StringBuilder tempString = new StringBuilder();
-
-
+    private Random rand = new Random(5);
     private void run() {
         try {
             receiver = new ServerSocket(9999);
@@ -39,7 +39,8 @@ public class Receiver {
                         System.out.println("Receiver - " + packet + " ( Duplicate Data )");
                     }
                     //below code is written to intentionally wrong seq to give wrong or no acknowledgement. Done for demo purpose
-                    if (i < 3) {
+                    int randNum = rand.nextInt(3) + 1;
+                    if (i < randNum) {
                         out.writeObject(String.valueOf(sequence));
                         i++;
                     } else {
